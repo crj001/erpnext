009eda6851663b7803950252b4a8b670e6e8bb15
crj001 <759681304@qq.com>
2026-04-23 11:42:50 +0800

    feat(会计科目): 添加中国新会计准则科目模板

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -6,7 +6,7 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils.user import is_website_user
 
-__version__ = "16.8.2+cus260420"
+__version__ = "16.8.2+cus260422"
 
 
 def get_default_company(user=None):
